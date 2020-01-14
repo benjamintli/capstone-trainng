3079289a77f39b2b0889f6839e7f44771cb79291
Ubuntu <capstone@training-cuda-enabled.5r3j2cx1s1guzcefa4xqv3izqf.bx.internal.cloudapp.net>
2020-01-14 03:44:38 +0000

    woooo new build

--- data/valid_images/lmao.py
+++ data/valid_images/lmao.py
@@ -3,11 +3,6 @@ import glob
 for file in glob.glob("*.txt"):
     with open(file, "r") as f:
         lines = f.readlines()
-    with open(file, "w") as f:
         for line in lines:
-            if line[0:2] == '28':
-                line = '1' + line[2:]
-            if line[0:2] == '43':
-                line = '3' + line[2:]
-            f.write(line)
-
+            if line[0] == '0':
+                print(line)
